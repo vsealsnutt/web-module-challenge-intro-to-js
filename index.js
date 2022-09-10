6eb2cc8af3309699d38638ed382acccd2e41e33b
Vanessa Seals-Nutt <v.sealsnutt@gmail.com>
2022-09-10 01:23:42 -0400

    completed task 7

--- index.js
+++ index.js
@@ -270,8 +270,8 @@ Using the annoyingSong function below do the following:
 4. Each time the annoyingSong is run from this loop, it should console.log the string that was returned. 
 */
 
-function annoyingSong(/*add your code here*/){
-      /*add your code here*/
+function annoyingSong(number){
+      console.log (`${number} bottles of soda on the wall, ${number} bottles of soda, take one down pass it around ${number - 1} bottles of soda on the wall`);
 }
 
 
@@ -290,10 +290,20 @@ Using the grade function below do the following:
  below should return 'you got an F'
 */
 
-function grade(/*Your Code here */){
-/*Your Code here */
+function grade(score){
+if (score >= 90) {
+  return 'you got an A';
+} else if (score >= 80) {
+  return 'you got a B';
+} else if (score >= 70) {
+  return 'you got a C';
+} else if (score >= 60) {
+  return 'you got a D';
+} else {
+  return 'you got an F';
 }
-
+}
+console.log(grade(82));
 
 
 /*💪💪💪💪💪💪💪💪💪💪 Stretch 💪💪💪💪💪💪💪💪💪💪*/
